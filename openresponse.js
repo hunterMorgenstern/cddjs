10580e32f3d6893658c208bb0df95dac8724999f
hunterBaller <hunter.morgenstern@gmail.com>
2022-07-09 08:15:15 -0700

    writing to csv

--- openresponse.js
+++ openresponse.js
@@ -1,4 +1,4 @@
-[
+const response = [
   {
     text: "\n\n1. The person is against the farm tax.\n2. The sentiment is negative.\n3. The person's points are that the tax will make it hard for farmers to pay for their equipment, and that they may have to sell part of their farm.\n4. The person supports their points with facts.\n5. The facts are that the tax will make it hard for farmers to pay for their equipment, and that they may have to sell part of their farm.\n6. There are no anecdotes.\n7. There are no quotes.\n8. The claims are that the tax will make it hard for farmers to pay for their equipment, and that they may have to sell part of their farm.\n9. The premises are that the tax will make it hard for farmers to pay for their equipment, and that they may have to sell part of their farm.\n10. The topic is the farm tax.",
     index: 0,
@@ -6,3 +6,36 @@
     finish_reason: "stop",
   },
 ];
+
+const lines = response[0].text.split("\n");
+// remove empty strings from  lines
+const res = lines.filter((line) => line.length > 0);
+console.log("!!!lines", res);
+
+import { createWriteStream } from "fs";
+import { stringify } from "csv-stringify";
+// const db = require("./db");
+const filename = "saved_from_db.csv";
+const writableStream = createWriteStream(filename);
+
+const columns = [
+  "year_month",
+  "month_of_release",
+  "passenger_type",
+  "direction",
+  "sex",
+  "age",
+  "estimate",
+];
+
+const stringifier = stringify({ header: true, columns: columns });
+// db.each(`select * from migration`, (error, row) => {
+//   if (error) {
+//     return console.log(error.message);
+//   }
+// res.forEach((line) => {
+stringifier.write(res);
+// });
+// });
+stringifier.pipe(writableStream);
+console.log("Finished writing data");
