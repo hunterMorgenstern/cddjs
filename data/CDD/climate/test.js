2a337367b40127028372829b9298eb179c0dc967
hunterBaller <hunter.morgenstern@gmail.com>
2022-08-01 16:09:57 -0700

    csv2json and test run of sample data

---
+++ data/CDD/climate/test.js
@@ -0,0 +1,16 @@
+const sample = [
+  {
+    order: "2",
+    "author": "steviek6",
+    "time": "14:48:00-15:33:00",
+    text: " so i'll start off by saying that i think that the use of fossil fuels has gotten us from the 20th, in the 19th century through the 20th century now to the 21st century. okay, and and maybe it's time to realize that all of that buildup of carbon in the environment is causing some of the issues that we're seeing today in globe, as far as weather is concerned. so, so i think that yeah, it's time to stop. it's time to get people on board with this and it's time to bring together the world and see if everyone agrees. but i think people are agreeing. i think that the movement is in this direction for sure.",
+  },
+  {
+    order: "3",
+    "author": "Francis",
+    time: "15:33:00-15:59:00",
+    text: " i definitely like the idea of phasing fossil fuels out and stuff like that. i don't think that it is something that can happen overnight. so i like the idea that they have timelines and place. i feel like a certain type of compromise is needed in certain areas, but with the idea of progressing and moving forward, i think it's all a good idea.",
+  },
+];
+
+export default sample;
