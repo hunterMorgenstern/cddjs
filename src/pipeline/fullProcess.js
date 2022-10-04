62a099dac83a3600cfd64f486195630ea01c5696
Daniel Hu <daniel@fileread.com>
2022-10-04 18:30:43 -0400

    work

--- src/pipeline/fullProcess.js
+++ src/pipeline/fullProcess.js
@@ -5,6 +5,38 @@ import formatAndWriteToCsv from "./cddCSV.js";
 import factsAndAnecdotes from "./cddOrig.js";
 import fs from 'fs'
 
+const dudLines = [
+  'is okay',
+  'is bad',
+  'is the best thing ever',
+  'is the worst thing ever'
+]
+
+async function cleanupArguments(argsListed, proposal) {
+  const newJson = JSON.parse(JSON.stringify(argsListed))
+  // force the completion to have at least 5 lines
+  for (var i = 0; i < argsListed.length; i++) {
+    const line = newJson[i]['response']['completion']
+    const lines = line.split('\n')
+    const newLines = []
+    for (var j = 0; j < lines.length; j++) {
+      if (lines[j].length > 0) {
+        newLines.push(lines[j])
+      }
+    }
+    if (newLines.length == 1 && newLines[0].length >= 1 && newLines[0][0] != '1') {
+      newLines[0] = `1. ${newLines[0]}`
+    }
+    let start = 0
+    while (newLines.length < 5) {
+      newLines.push(`${newLines.length + 1}. ${proposal} ${dudLines[start]}`)
+      start += 1;
+    }
+    newJson[i]['response']['completion'] = newLines.join('\n');
+  }
+  return newJson
+}
+
 async function proposalSummarization(transcriptCSV, resultsRepo, proposals) {
   const jsonArray = await csv().fromFile(transcriptCSV);
 
@@ -19,8 +51,9 @@ async function proposalSummarization(transcriptCSV, resultsRepo, proposals) {
 
   // pros and cons
   proposals.forEach(async (proposal) => {
+    const argumentsListedCleaned = await cleanupArguments(argumentsListed, proposal.text)
     await processProsTranscript(
-      argumentsListed,
+      argumentsListedCleaned,
       `${resultsRepo}/pros/`,
       proposal.text,
       proposal.number
